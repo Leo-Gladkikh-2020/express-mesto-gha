7681e3901560fd75dfe0fa5a0d9c6850b59831c8
Leonid Gladkikh <leogladkikh@yandex.ru>
2022-03-23 13:02:41 +0200

    feat: add controllers and routers for users

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const routerUsers = require('./routes/users');
+const routerCards = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 
@@ -8,6 +10,9 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/users', routerUsers);
+app.use('/cards', routerCards);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
---
+++ routes/users.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const {
+  getUsers, getUserById, getCurrentUser, updateUserInfo, updateUserAvatar,
+} = require('../controllers/users');
+
+router.get('/', getUsers);
+router.get('/:userId', getUserById);
+router.get('/me', getCurrentUser);
+router.patch('me', updateUserInfo);
+router.patch('me/avatar', updateUserAvatar);
+
+module.exports = router;
